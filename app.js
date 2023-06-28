0944cc3138313bce1756611d90d46dcabc9c86e0
Adil Saju <adilsaju@gmail.com>
2023-06-28 14:03:41 -0700

    .

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
+const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,6 +17,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+
+//DATABASE SETUP
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useCreateIndex: true } )
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true } )
+// const db = mongoose.connection 
+// db.on('error',(error)=>console.error(error))
+// db.once('open',()=>console.error('connected to database'))
+
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const cors = require("cors")
-
-router.use(cors())
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
