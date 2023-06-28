7be5250247d8abb42dff7f5e6f195054cb467df3
Adil Saju <adilsaju@gmail.com>
2023-06-28 12:55:18 -0700

    backend upd

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const cors = require("cors")
+
+router.use(cors())
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  // res.render('index', { title: 'Express' });
+  console.log("hello");
+  res.json("ab")
 });
 
+router.get('/test', function (req, res, next) {
+  console.log("hello");
+});
 module.exports = router;
