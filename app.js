a12edff7d9fe25f1c0c493192ed3e1e567f9e750
Adil Saju <adilsaju@gmail.com>
2023-06-28 17:15:29 -0700

    chatgpt graphql code

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose')
 const cors = require('cors')
+const { ApolloServer, gql } = require('apollo-server-express');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mainRoute = require('./routes/mainRoute');
+
+const {roomSchema, messageSchema}  = require('./models/chatModel')
 
 
 var app = express();
@@ -21,10 +25,122 @@ app.use(cors())
 
 //DATABASE SETUP
 // mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useCreateIndex: true } )
-// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true } )
-// const db = mongoose.connection 
-// db.on('error',(error)=>console.error(error))
-// db.once('open',()=>console.error('connected to database'))
+mongoose.connect('mongodb://localhost/chatapp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection 
+db.on('error',(error)=>console.error(error))
+db.once('open',()=>console.error('connected to database'))
+
+// Define Mongoose models
+const Room = mongoose.model('Room', roomSchema);
+const Message = mongoose.model('Message', messageSchema);
+
+
+// GraphQL schema
+const typeDefs = gql`
+  type Room {
+    id: ID!
+    room: String!
+    employer: Employer
+    technician: Technician
+    job: Job
+    room_created: String!
+  }
+
+  type Employer {
+    id: ID!
+    name: String!
+  }
+
+  type Technician {
+    id: ID!
+    name: String!
+  }
+
+  type Job {
+    id: ID!
+    title: String!
+  }
+
+  type Message {
+    id: ID!
+    room: Room!
+    sender: User!
+    message: String!
+    date: String!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+  }
+
+  type Query {
+    rooms: [Room]
+    room(id: ID!): Room
+    messages(roomId: ID!): [Message]
+  }
+
+  type Mutation {
+    createRoom(room: String!, employerId: ID, technicianId: ID, jobId: ID): Room
+    sendMessage(roomId: ID!, senderId: ID!, message: String!): Message
+  }
+`;
+
+// GraphQL resolvers
+const resolvers = {
+  Query: {
+    rooms: () => Room.find(),
+    room: (_, { id }) => Room.findById(id),
+    messages: (_, { roomId }) => Message.find({ room_id: roomId }),
+  },
+  Mutation: {
+    createRoom: (_, { room, employerId, technicianId, jobId }) => {
+      const newRoom = new Room({
+        room,
+        employer_id: employerId,
+        technician_id: technicianId,
+        job_id: jobId,
+      });
+      return newRoom.save();
+    },
+    sendMessage: (_, { roomId, senderId, message }) => {
+      const newMessage = new Message({
+        room_id: roomId,
+        sender_id: senderId,
+        message,
+      });
+      return newMessage.save();
+    },
+  },
+  Room: {
+    employer: (parent) => Employer.findById(parent.employer_id),
+    technician: (parent) => Technician.findById(parent.technician_id),
+    job: (parent) => Job.findById(parent.job_id),
+  },
+  Message: {
+    room: (parent) => Room.findById(parent.room_id),
+    sender: (parent) => {
+      if (parent.docModel === 'employer') {
+        return Employer.findById(parent.sender_id);
+      } else if (parent.docModel === 'technician') {
+        return Technician.findById(parent.sender_id);
+      }
+    },
+  },
+};
+
+
+
+async function startServer1() {
+// Create Apollo Server
+const server1 = new ApolloServer({ typeDefs, resolvers });
+await server1.start();
+// Mount Apollo Server on the Express app
+server1.applyMiddleware({ app });
+}
 
 
 app.use('/', indexRouter);
@@ -37,5 +153,8 @@ app.use((req, res, next) => {
       error: '404 Page: Not found',
     });
   });
+
+startServer1();
+
 
 module.exports = app;
